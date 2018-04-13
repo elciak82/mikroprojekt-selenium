4ad253762db4d1f9b6626059db3ad59d3ee9ea4c
Brokulli <respondek.patrycja@gmail.com>
2018-04-13 11:13:51 +0200

    poprawki kosmetyczne

--- testy.py
+++ testy.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 import unittest
 from time import sleep
 
-//zmienić na GIVEN WHEN THAN Metoda BDD
+# zmienić na GIVEN WHEN THAN Metoda BDD
 url = 'https://zrabatowani.pl/'
 logo_css = 'logo-top '
 registration_css = 'no_p'
